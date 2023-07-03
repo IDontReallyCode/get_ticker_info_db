f7a342df87bbdd2fd28c3ff53591086ed930b506
IDontReallyCode <92130644+IDontReallyCode@users.noreply.github.com>
2023-07-03 12:30:50 -0500

    It works for all tickers that are still listed

--- main_just_list_all_tickers.py
+++ main_just_list_all_tickers.py
@@ -22,6 +22,8 @@ Example:
     For example, ZZZ, and ZZZ1
     
     The standard for index tickers is different also. So I may need to "eff around to find out".
+    
+    The list contains tickers that have since dissapeared because M&A or delistings.
 """
 
 import os
@@ -29,6 +31,10 @@ import polars as pl
 import psycopg2
 import pickle
 import config
+from tda.auth import easy_client
+from tda.client import Client
+from config import CONSUMER_KEY, REDIRECT_URI, JSON_PATH
+import time
 
 # Get some SQL connections ready
 with open('sql.private', 'rb') as f:
@@ -47,7 +53,7 @@ def SQL_CONNECT(dbname = 'optiondb', port='5432', password=MYSQLPWD):
     return cnxn
 
 
-def SQL_GET_TICKERS_WITH_DETAILS()-> list:
+def SQL_SELECT_TICKERS_WITH_DETAILS()-> list:
     conn = SQL_CONNECT()
     
     # create a new cursor object
@@ -63,23 +69,86 @@ def SQL_GET_TICKERS_WITH_DETAILS()-> list:
     return listdone
 
 
+def SQL_INSERT_TICKER_DETAILS(cnxn:psycopg2.extensions.connection, tickdata:dict):
+    # Connect to Database
+    # cnxn = SQL_CONNECT()
+    cursor = cnxn.cursor()
+   
+    sql_InsertStatement = 'INSERT INTO public.ticker_detail (ticker, cusip, description, assettype, exchange) VALUES (%s, %s, %s, %s, %s)'
+
+    if 'cusip' not in tickdata:
+        tickdata['cusip'] = ''
+        
+    try:
+        cursor.execute(sql_InsertStatement,
+            (tickdata['symbol'], tickdata['cusip'], tickdata['description'], tickdata['assetType'], tickdata['exchange'])
+            )
+    except psycopg2.Error as e:
+        print(f"Error inserting: {e}")
+
+    cnxn.commit()  
+    cursor.close()
+      
+
+
 def remove_extension(filename: str) -> str:
-    return '.'.join(filename.split('.')[:-1])
+    ticker = '.'.join(filename.split('.')[:-1])
+    
+    return ''.join(char for char in ticker if not char.isdigit())
 
 
 def main():
     # read the list of file names
-    tickerlist = pl.read_ipc('list_all_ticker_files.ipc')
+    df = pl.read_ipc('list_all_ticker_files.ipc')
 
     # get list of tickers for which the work is done already
-    listdone = SQL_GET_TICKERS_WITH_DETAILS()
+    listdone = SQL_SELECT_TICKERS_WITH_DETAILS()
     
     # extract ticker from filename
-    tickerlist = tickerlist.with_columns([pl.col("filename").apply(remove_extension).alias("ticker")])
+    df = df.with_columns([pl.col("filename").apply(remove_extension).alias("ticker")])
     
     # remove those that are done from the list
-    tickerlist = tickerlist.filter(pl.col('ticker').is_in(listdone).is_not())    
+    df = df.filter(pl.col('ticker').is_in(listdone).is_not()) 
     
+    tickerlist = df['ticker'].unique().to_list()
+    
+    # ticklist = tickerlist.iter_rows()   
+    # tickers = df['ticker'].to_list()
+    # tickers_string = ','.join(tickers)
+    
+    cnxn = SQL_CONNECT()
+    
+    for i in range(0, len(tickerlist), 10):
+        # for ticker in tickerlist:
+        start_time = time.time()        # this is used to throttle the process to stay within the limits of TDA
+    
+        # first, make sure I have the ticker, and not some notation of it
+        letters_only = ','.join(tickerlist[i:i+10])
+    
+        c = easy_client(        api_key=CONSUMER_KEY,        redirect_uri=REDIRECT_URI,        token_path=JSON_PATH)
+        r = c.search_instruments(symbols=letters_only, projection=Client.Instrument.Projection.SYMBOL_SEARCH)
+        
+        for key in r.json().keys():
+            SQL_INSERT_TICKER_DETAILS(cnxn, r.json()[key])
+        
+        # if not bool(r.json()):
+        #     # the search failed. Try for an index
+        #     indexticker = '$'+ticker+'.X'
+        #     r = c.search_instruments(symbols=indexticker, projection=Client.Instrument.Projection.SYMBOL_SEARCH)
+        #     if not bool(r.json()):
+        #         # not an index either
+        #         # Leave it be, it will come out an "not done yet later"
+        #         pass
+        #     else:
+        #         pausefordebug=1
+        # else:
+        #     SQL_INSERT_TICKER_DETAILS(cnxn, r.json()[ticker])
+        
+        delay = 0.6 - (time.time() - start_time)
+        if delay>0:
+            time.sleep(delay)
+
+        pass
     
     pass
 
